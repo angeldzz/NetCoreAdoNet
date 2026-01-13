c736abab43e070daeedd87d0d2dc009442fe1aac
angeldzz <pintodiazangel@gmail.com>
2026-01-13 12:46:48 +0100

    Add project files.

---
+++ NetCoreAdoNet/Form01PrimerAdo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NetCoreAdoNet
+{
+    public partial class Form01PrimerAdo : Form
+    {
+        public Form01PrimerAdo()
+        {
+            InitializeComponent();
+        }
+    }
+}
