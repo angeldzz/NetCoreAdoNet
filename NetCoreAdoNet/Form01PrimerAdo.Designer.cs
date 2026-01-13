b4916b70373f982fea75da63d1ca071d5d02964d
angeldzz <pintodiazangel@gmail.com>
2026-01-13 13:13:19 +0100

    Conectar y desconectar con AdoNet a SQL server, uso de excepciones

--- NetCoreAdoNet/Form01PrimerAdo.Designer.cs
+++ NetCoreAdoNet/Form01PrimerAdo.Designer.cs
@@ -28,12 +28,139 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form01PrimerAdo";
+            btnConectar = new Button();
+            btnDesconectar = new Button();
+            btnRead = new Button();
+            label1 = new Label();
+            lstApellidos = new ListBox();
+            lstColumnas = new ListBox();
+            label2 = new Label();
+            lstTipos = new ListBox();
+            label3 = new Label();
+            lblConexion = new Label();
+            SuspendLayout();
+            // 
+            // btnConectar
+            // 
+            btnConectar.Location = new Point(12, 25);
+            btnConectar.Name = "btnConectar";
+            btnConectar.Size = new Size(86, 36);
+            btnConectar.TabIndex = 0;
+            btnConectar.Text = "Conectar";
+            btnConectar.UseVisualStyleBackColor = true;
+            btnConectar.Click += btnConectar_Click;
+            // 
+            // btnDesconectar
+            // 
+            btnDesconectar.Location = new Point(12, 67);
+            btnDesconectar.Name = "btnDesconectar";
+            btnDesconectar.Size = new Size(86, 36);
+            btnDesconectar.TabIndex = 1;
+            btnDesconectar.Text = "Desconectar";
+            btnDesconectar.UseVisualStyleBackColor = true;
+            btnDesconectar.Click += btnDesconectar_Click;
+            // 
+            // btnRead
+            // 
+            btnRead.Location = new Point(12, 109);
+            btnRead.Name = "btnRead";
+            btnRead.Size = new Size(86, 36);
+            btnRead.TabIndex = 2;
+            btnRead.Text = "READ";
+            btnRead.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(134, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Apellidos";
+            // 
+            // lstApellidos
+            // 
+            lstApellidos.FormattingEnabled = true;
+            lstApellidos.Location = new Point(134, 51);
+            lstApellidos.Name = "lstApellidos";
+            lstApellidos.Size = new Size(196, 334);
+            lstApellidos.TabIndex = 4;
+            // 
+            // lstColumnas
+            // 
+            lstColumnas.FormattingEnabled = true;
+            lstColumnas.Location = new Point(346, 51);
+            lstColumnas.Name = "lstColumnas";
+            lstColumnas.Size = new Size(196, 334);
+            lstColumnas.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(346, 14);
+            label2.Name = "label2";
+            label2.Size = new Size(61, 15);
+            label2.TabIndex = 5;
+            label2.Text = "Columnas";
+            // 
+            // lstTipos
+            // 
+            lstTipos.FormattingEnabled = true;
+            lstTipos.Location = new Point(564, 51);
+            lstTipos.Name = "lstTipos";
+            lstTipos.Size = new Size(196, 334);
+            lstTipos.TabIndex = 8;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(564, 14);
+            label3.Name = "label3";
+            label3.Size = new Size(35, 15);
+            label3.TabIndex = 7;
+            label3.Text = "Tipos";
+            // 
+            // lblConexion
+            // 
+            lblConexion.AutoSize = true;
+            lblConexion.Location = new Point(28, 403);
+            lblConexion.Name = "lblConexion";
+            lblConexion.Size = new Size(70, 15);
+            lblConexion.TabIndex = 9;
+            lblConexion.Text = "lstConexion";
+            // 
+            // Form01PrimerAdo
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(lblConexion);
+            Controls.Add(lstTipos);
+            Controls.Add(label3);
+            Controls.Add(lstColumnas);
+            Controls.Add(label2);
+            Controls.Add(lstApellidos);
+            Controls.Add(label1);
+            Controls.Add(btnRead);
+            Controls.Add(btnDesconectar);
+            Controls.Add(btnConectar);
+            Name = "Form01PrimerAdo";
+            Text = "Form01PrimerAdo";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnConectar;
+        private Button btnDesconectar;
+        private Button btnRead;
+        private Label label1;
+        private ListBox lstApellidos;
+        private ListBox lstColumnas;
+        private Label label2;
+        private ListBox lstTipos;
+        private Label label3;
+        private Label lblConexion;
     }
 }
--- NetCoreAdoNet/Form01PrimerAdo.cs
+++ NetCoreAdoNet/Form01PrimerAdo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,9 +11,39 @@ namespace NetCoreAdoNet
 {
     public partial class Form01PrimerAdo : Form
     {
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+        string conectionString;
+
         public Form01PrimerAdo()
         {
             InitializeComponent();
+            this.conectionString = @"Data Source=""LOCALHOST \DEVELOPER"";Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Trust Server Certificate=True";
+            this.cn = new SqlConnection(this.conectionString);
+            this.com = new SqlCommand();
+        }
+
+        private void btnConectar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (this.cn.State == ConnectionState.Closed)
+                {
+                    this.cn.Open();
+                }
+                this.lblConexion.BackColor = Color.LightGreen;
+            }
+            catch (Exception ex)
+            {
+                this.lblConexion.Text = "Error " + ex.ToString();
+            }
+        }
+
+        private void btnDesconectar_Click(object sender, EventArgs e)
+        {
+            this.cn.Close();
+            this.lblConexion.BackColor = Color.LightCoral;
         }
     }
 }
